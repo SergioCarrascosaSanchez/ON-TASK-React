f21b4529737c2078ac62a953a7d346da2bc9cc71
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-08 16:03:53 +0200

    Login and Signup ReactRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './pages/Login';
+import SignupPage from './pages/Signup';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+        <Route path ='/login' element={<LoginPage/>}></Route>
+        <Route exact path ='/sign-up' element={<SignupPage/>}></Route>
+      </Routes>
   );
 }
 

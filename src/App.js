e954e3bc8ac7d9e0abc9efd31440fe576028794c
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-17 13:39:07 +0200

    New Routes + remove Strict rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
 import MainPage from './pages/Main';
 import {Routes, Route} from 'react-router-dom';
+import UserMainPage from './pages/UserMain';
+import Prueba from './pages/Pruebas';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path ='/login' element={<LoginPage/>}></Route>
         <Route exact path ='/sign-up' element={<SignupPage/>}></Route>
         <Route exact path ='/' element={<MainPage/>}></Route>
+        <Route path='/users/:username' element={<UserMainPage/>}></Route>
+        <Route path='/pruebas' element={<Prueba />}></Route>
       </Routes>
   );
 }

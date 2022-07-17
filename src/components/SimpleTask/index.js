1ced0df7b5ac284feb11df5426dfe415cd600ccf
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-07-17 13:36:17 +0200

    add Id prop SimpleTask + Links + Better styling

--- src/components/SimpleTask/index.js
+++ src/components/SimpleTask/index.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import './styles.css'
 export default class SimpleTask extends Component {
   
     constructor(props){
         super()
-        console.log(props)
         this.state={
+            id : props.id,
             title : props.title,
             description: props.description
         }
     }
 
     render() {
+    const url = '/tasks/'+this.state.id
     return (
-        <Card className="CardStyled">
-            <Card.Header className="text-justify"> <h3>{this.state.title}</h3> </Card.Header>
-            <Card.Body> {this.state.description} </Card.Body>
-        </Card>
+        <Link to={url} style={{ textDecoration: 'none', color:'black' }}>
+            <Card className="CardStyled">
+                <Card.Header> <h5>{this.state.title}</h5> </Card.Header>
+                <Card.Body> {this.state.description} </Card.Body>
+            </Card>
+        </Link>
     )
   }
 }

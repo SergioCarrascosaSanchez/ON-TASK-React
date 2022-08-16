c070a5d03febecde416f9556c42ec477fd0e77c9
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-08-16 20:46:04 +0200

    SimpleNavBar new name + Logout

--- src/components/SimpleNavBar/index.js
+++ src/components/SimpleNavBar/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import {Navbar} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default class SimpleNavBar extends Component {
+
   render() {
     return (
         <Navbar bg="primary" variant="dark">
+          <div className="container-fluid">
             <Navbar.Brand>
-                <span className="navbarBrand">Gestor de tareas</span>
+              <Link to="/" style={{ textDecoration: 'none', color:'white' }}>
+                <span className="navbarBrand">ON TASK</span>
+              </Link>
             </Navbar.Brand>
+            <Navbar.Text className="d-flex me-5">
+              <Link to="/" style={{ textDecoration: 'none', color:'white' }}>
+                Log out
+              </Link>
+            </Navbar.Text>
+          </div>
         </Navbar>
     )
   }

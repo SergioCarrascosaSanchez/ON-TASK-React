f4abd358c56cf8513e1ac81f04d3442d3737b825
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-08-17 15:17:11 +0200

    Remove LocalStorage when logout + add groups to LocalStorage

--- src/components/SimpleNavBar/index.js
+++ src/components/SimpleNavBar/index.js
@@ -4,6 +4,12 @@ import { Link } from 'react-router-dom'
 
 export default class SimpleNavBar extends Component {
 
+  handleLogout = () => {
+    window.localStorage.removeItem('groups')
+    window.localStorage.removeItem('user')
+    window.localStorage.removeItem('group')
+  }
+
   render() {
     return (
         <Navbar bg="primary" variant="dark">
@@ -13,7 +19,7 @@ export default class SimpleNavBar extends Component {
                 <span className="navbarBrand">ON TASK</span>
               </Link>
             </Navbar.Brand>
-            <Navbar.Text className="d-flex me-5">
+            <Navbar.Text className="d-flex me-5" onClick={this.handleLogout}>
               <Link to="/" style={{ textDecoration: 'none', color:'white' }}>
                 Log out
               </Link>

9836d1665612fc0044ff1c69b42521bb231d28eb
Sergio Carrascosa Sánchez <sercarsa11@gmail.com>
2022-09-03 13:38:18 +0200

    localSto to context NavBar + profileLinkButton

--- src/components/SimpleNavBar/index.js
+++ src/components/SimpleNavBar/index.js
@@ -1,32 +1,54 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import {Navbar} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import CurrentContext from '../../context/currentContext'
+import UserLoginContext from '../../context/userLoginContext'
+import * as Icon from 'react-bootstrap-icons';
 
-export default class SimpleNavBar extends Component {
+export default function SimpleNavBar(){
+  const userContext = useContext(UserLoginContext)
+  const currentContext = useContext(CurrentContext)
 
-  handleLogout = () => {
-    window.localStorage.removeItem('groups')
+  const handleLogout = () => {
+    userContext.setUsername("")
+    userContext.setToken("")
+    userContext.setGroups([])
+    currentContext.setGroup("")
+    /*window.localStorage.removeItem('groups')
     window.localStorage.removeItem('user')
     window.localStorage.removeItem('group')
-    window.localStorage.removeItem('token')
+    window.localStorage.removeItem('token')*/
   }
 
-  render() {
-    return (
-        <Navbar bg="primary" variant="dark">
-          <div className="container-fluid">
-            <Navbar.Brand>
-              <Link to="/" style={{ textDecoration: 'none', color:'white' }}>
-                <span className="navbarBrand">ON TASK</span>
-              </Link>
-            </Navbar.Brand>
-            <Navbar.Text className="d-flex me-5" onClick={this.handleLogout}>
-              <Link to="/" style={{ textDecoration: 'none', color:'white' }}>
-                Log out
-              </Link>
-            </Navbar.Text>
-          </div>
-        </Navbar>
-    )
+  const profileLink = () => {
+    if(userContext.username !== ""){
+      return(
+        <Link to={"/users/"+userContext.username}>
+          <Icon.PersonCircle color="white" size={25} className=""/>
+        </Link>
+      )
+    }
+    else{
+      return (<></>)
+    }
   }
+
+  
+  return (
+      <Navbar bg="primary" variant="dark">
+        <div className="container-fluid">
+          <Navbar.Brand>
+            <Link to="/" style={{ textDecoration: 'none', color:'white' }}>
+              <span className="navbarBrand">ON TASK</span>
+            </Link>
+          </Navbar.Brand>
+          {profileLink()}
+          <Navbar.Text className="d-flex me-5" onClick={handleLogout}>
+            <Link to="/" style={{ textDecoration: 'none', color:'white' }}>
+              Log out
+            </Link>
+          </Navbar.Text>
+        </div>
+      </Navbar>
+  )
 }
